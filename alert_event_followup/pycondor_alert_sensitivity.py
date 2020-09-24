810985d8871b7a571bd9977d7eaa46a800745b5f
Alex Pizzuto <pizzuto@wisc.edu>
2020-09-24 10:51:26 -0500

    Add scripts for time integrated and transient analyses

---
+++ alert_event_followup/pycondor_alert_sensitivity.py
@@ -0,0 +1,34 @@
+import pycondor, argparse, sys, os
+from glob import glob
+import numpy as np
+import pandas as pd
+
+error = '/scratch/apizzuto/fast_response/condor/error'
+output = '/scratch/apizzuto/fast_response/condor/output'
+log = '/scratch/apizzuto/fast_response/condor/log'
+submit = '/scratch/apizzuto/fast_response/condor/submit'
+
+job = pycondor.Job('sensitivity_fastresponse_alerts','./alert_event_sensitivity.py',
+			error=error,
+			output=output,
+			log=log,
+			submit=submit,
+            getenv=True,
+            universe='vanilla',
+			verbose=2, 
+			request_memory=8000,
+            request_cpus=5,
+			extra_lines= ['should_transfer_files = YES', 'when_to_transfer_output = ON_EXIT', 'Requirements =  (Machine != "node128.icecube.wisc.edu")']
+			)
+
+skymap_files = glob('/data/ana/realtime/alert_catalog_v2/fits_files/Run1*.fits.gz')
+
+for index in range(len(skymap_files)):
+    for deltaT in np.array([1000., 2.*86400.]):
+        for smear_str in [' --smear', '']:
+            add_str = ' --ntrials=100' if deltaT > 100000. else ''
+            job.add_arg('--deltaT={} --index={}'.format(deltaT, index) + add_str + smear_str)
+
+dagman = pycondor.Dagman('alert_event_fra_sensitivity', submit=submit, verbose=2)
+dagman.add_job(job)
+dagman.build_submit()
---
+++ alert_event_followup/pycondor_manual_lumi.py
@@ -0,0 +1,66 @@
+import pycondor, argparse, sys, os
+from glob import glob
+import numpy as np
+import pandas as pd
+
+error = '/scratch/apizzuto/fast_response/condor/error'
+output = '/scratch/apizzuto/fast_response/condor/output'
+log = '/scratch/apizzuto/fast_response/condor/log'
+submit = '/scratch/apizzuto/fast_response/condor/submit'
+
+low_mem_job = pycondor.Job('low_mem_calc_ts_dists','/data/user/apizzuto/fast_response_skylab/fast-response/trunk/alert_event_followup/first_full_ts_sampling.py',
+			error=error,
+			output=output,
+			log=log,
+			submit=submit,
+            getenv=True,
+            universe='vanilla',
+			verbose=2, 
+			request_memory=4000,
+            request_cpus=1,
+			extra_lines= ['should_transfer_files = YES', 'when_to_transfer_output = ON_EXIT', 'Requirements =  (Machine != "node128.icecube.wisc.edu")']
+			)
+
+high_mem_job = pycondor.Job('high_mem_calc_ts_dists','/data/user/apizzuto/fast_response_skylab/fast-response/trunk/alert_event_followup/first_full_ts_sampling.py',
+			error=error,
+			output=output,
+			log=log,
+			submit=submit,
+            getenv=True,
+            universe='vanilla',
+			verbose=2, 
+			request_memory=8000,
+            request_cpus=1,
+			extra_lines= ['should_transfer_files = YES', 'when_to_transfer_output = ON_EXIT', 'Requirements =  (Machine != "node128.icecube.wisc.edu")']
+			)
+
+for delta_t in [1000., 2.*86400.]:
+    for manual_lumi in np.logspace(49, 60, 45):
+    #for manual_lumi in np.logspace(60, 64, 17):
+        for density in np.logspace(-11., -6., 21)[:]:
+            for lumi in ['SC', 'LG']:
+                for evol in ['MD2014SFR']:
+                    if density * manual_lumi * delta_t > 3e54:
+                        continue
+                    elif density * manual_lumi * delta_t < 1e47:
+                        continue
+                    else:
+                        N = 500 if density < 1e-7 else 40
+                        if density > 3e-8:
+                            high_mem_job.add_arg('--delta_t={} --density={} --LF={} --n={} --evol={} --manual_lumi={}'.format(delta_t, density, lumi, N, evol, manual_lumi))
+                        else:
+                            low_mem_job.add_arg('--delta_t={} --density={} --LF={} --n={} --evol={} --manual_lumi={}'.format(delta_t, density, lumi, N, evol, manual_lumi))
+
+#for delta_t in [1000., 2.*86400.]:
+#    for density in np.logspace(-11., -6., 21)[:]:
+#        for lumi in ['SC', 'LG']:
+#            for evol in ['MD2014SFR']:
+#                N = 500 if density < 1e-7 else 40
+#                job.add_arg('--delta_t={} --density={} --LF={} --n={} --evol={}'.format(delta_t, density, lumi, N, evol))                        
+
+dagman = pycondor.Dagman('ts_distributions', submit=submit, verbose=2)
+
+dagman.add_job(low_mem_job)
+dagman.add_job(high_mem_job)
+
+dagman.build_submit()
