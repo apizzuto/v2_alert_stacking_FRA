829d37f93bdadf2daf0942f8affa3a64a50589d2
Alex Pizzuto <pizzuto@wisc.edu>
2021-01-07 12:25:51 -0600

    A shell to remind me to finish this script

---
+++ examples/make_all_universe_plots.py
@@ -0,0 +1,19 @@
+import argparse
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+mpl.style.use()
+
+from francis.universe.universe_plotter import UniversePlotter
+import matplotlib as mpl
+
+def make_analysis_plots(uni):
+    print('something')
+
+for time_window in ['steady', 2.*86400., 1000.]:
+    print("Beggining plots for ")
+    pass
+
+
+if __name__ == "__main__":
+    pass
