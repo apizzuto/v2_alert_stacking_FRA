dcbf0da7877f694ddd2cefb84ae802bd4eef9300
Alex Pizzuto <pizzuto@wisc.edu>
2021-03-25 16:01:47 -0500

    Time integrated f_path for condor

--- examples/cluster_jobs/steady/pycondor_background_alert_steady.py
+++ examples/cluster_jobs/steady/pycondor_background_alert_steady.py
@@ -2,31 +2,33 @@ import pycondor, argparse, sys, os
 from glob import glob
 import numpy as np
 import pandas as pd
+import francis.utils as utils
+f_path = utils.get_francis_path()
 
 error = '/scratch/apizzuto/fast_response/condor/error'
 output = '/scratch/apizzuto/fast_response/condor/output'
 log = '/scratch/apizzuto/fast_response/condor/log'
 submit = '/scratch/apizzuto/fast_response/condor/submit'
 
-job = pycondor.Job('background_fastresponse_alerts_steady','/data/user/apizzuto/fast_response_skylab/alert_event_followup/FRANCIS/francis/time_integrated_scripts/steady_background_trials.py',
-			error=error,
-			output=output,
-			log=log,
-			submit=submit,
-            getenv=True,
-            universe='vanilla',
-			verbose=2, 
-			request_memory=8000,
-            #request_cpus=5,
-			extra_lines= ['should_transfer_files = YES', 'when_to_transfer_output = ON_EXIT', 'Requirements =  (Machine != "node128.icecube.wisc.edu")']
-			)
+job = pycondor.Job('background_fastresponse_alerts_steady',
+    f_path + 'time_integrated_scripts/steady_background_trials.py',
+    error=error,
+    output=output,
+    log=log,
+    submit=submit,
+    getenv=True,
+    universe='vanilla',
+    verbose=2, 
+    request_memory=8000,
+    #request_cpus=5,
+    extra_lines= ['should_transfer_files = YES', 'when_to_transfer_output = ON_EXIT', 'Requirements =  (Machine != "node128.icecube.wisc.edu")']
+    )
+
 sky_files = glob('/data/ana/realtime/alert_catalog_v2/fits_files/Run1*.fits.gz')
 
 for rng in range(5):
-    for index in range(245, len(sky_files)):
-        for smear in [' --smear']: #, '']:
-            #if os.path.exists('/data/user/apizzuto/fast_response_skylab/alert_event_followup/background_ts_distributions/index_{}_time_{:.1f}.pkl'.format(index, deltaT)) and deltaT < 1000.:
-            #    continue
+    for index in range(len(sky_files)):
+        for smear in [' --smear']:
             job.add_arg('--rng={} --i={} --ntrials=200{}'.format(rng, index, smear))
 
 dagman = pycondor.Dagman('alert_event_fra_background_steady', submit=submit, verbose=2)
--- examples/cluster_jobs/steady/pycondor_signal_alert_steady.py
+++ examples/cluster_jobs/steady/pycondor_signal_alert_steady.py
@@ -2,32 +2,36 @@ import pycondor, argparse, sys, os
 from glob import glob
 import numpy as np
 import pandas as pd
+import francis.utils as utils
+f_path = utils.get_francis_path()
 
 error = '/scratch/apizzuto/fast_response/condor/error'
 output = '/scratch/apizzuto/fast_response/condor/output'
 log = '/scratch/apizzuto/fast_response/condor/log'
 submit = '/scratch/apizzuto/fast_response/condor/submit'
 
-job = pycondor.Job('signal_alerts_steady','/data/user/apizzuto/fast_response_skylab/alert_event_followup/FRANCIS/francis/time_integrated_scripts/signal_trials.py',
-			error=error,
-			output=output,
-			log=log,
-			submit=submit,
-            getenv=True,
-            universe='vanilla',
-			verbose=1, 
-			request_memory=8000,
-            #request_cpus=5,
-			extra_lines= ['should_transfer_files = YES', 'when_to_transfer_output = ON_EXIT']
-			)
+job = pycondor.Job('signal_alerts_steady',
+    f_path + 'time_integrated_scripts/signal_trials.py',
+    error=error,
+    output=output,
+    log=log,
+    submit=submit,
+    getenv=True,
+    universe='vanilla',
+    verbose=1, 
+    request_memory=8000,
+    #request_cpus=5,
+    extra_lines= ['should_transfer_files = YES', 
+        'when_to_transfer_output = ON_EXIT']
+    )
 
 sky_files = glob('/data/ana/realtime/alert_catalog_v2/fits_files/Run1*.fits.gz')
 
 for rng in range(10):
-    for index in range(245, len(sky_files)): #ONLY DO A FEW BEFORE WE GET THE FULL SAMPLE
+    for index in range(len(sky_files)):
         for gamma in [2.5]:
             for fit in [True, False]:
-                for smear in [' --smear']: #, '']:
+                for smear in [' --smear']:
                     add_str = ' --fit' if fit else ''
                     job.add_arg('--rng={} --i={} --ntrials=10 --g={}{}'.format(rng, index, gamma, smear) + add_str)
 
